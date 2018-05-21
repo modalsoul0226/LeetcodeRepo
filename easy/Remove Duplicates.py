c4c33f0eaf57d1a59bb5f7bf5fa03592a7253477
modalsoul0226 <modalsoul0226@gmail.com>
2018-05-22 00:49:16 +0800

    Finished one more question.

---
+++ easy/Remove Duplicates.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return head
+        
+        curr = head
+        while curr.next:
+            if curr.val == curr.next.val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        
+        return head
