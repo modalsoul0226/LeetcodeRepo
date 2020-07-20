c3605a2f24082ccef331b7e0a010dc34af1c0aa2
Xinze Zhao <modalsoul0226@gmail.com>
2020-07-20 16:04:19 -0400

    Create 144.binary.tree.preorder.traversal.cpp

---
+++ tree/144.binary.tree.preorder.traversal.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preorder;
+        if (!root) return preorder;
+
+        // init stack
+        stack<TreeNode *> nodes;
+        nodes.push(root);
+
+        // dfs
+        while (!nodes.empty()) {
+            // extract a node
+            TreeNode *node = nodes.top();
+            nodes.pop();
+
+            preorder.push_back(node->val);
+
+            // push children
+            if (node->right) nodes.push(node->right);
+            if (node->left) nodes.push(node->left);
+        }
+
+        return preorder;
+    }
+};
